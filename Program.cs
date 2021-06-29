ba95b4b03ed57b15e26c72f56bb4daf1b3edfe84
vavsn <41681585+vavsn@users.noreply.github.com>
2021-06-29 23:19:38 +0700

    Lesson 1.HW.Task 3.
    Lesson 1.Home work. Task 3. Calculation Fibonacci numbers.

--- Program.cs
+++ Program.cs
@@ -1,30 +1,58 @@
 ﻿using System;
 
-namespace Task1
+namespace Task2
 {
+
     class Program
     {
+
         /// <summary>
         /// класс для проведения тестирования
         /// </summary>
         public class TestCase
         {
-            public int X { get; set; }
-            public string Expected { get; set; }
+            public ulong X { get; set; }
+            public ulong Expected { get; set; }
             public Exception ExpectedException { get; set; }
         }
 
-        /// <summary>
-        /// метод проведения теста корректности работы процедуры PrimeNumber
-        /// </summary>
-        /// <param name="testCase"></param>
-        static void TestPrimeNumber(TestCase testCase)
+
+        static void TestNumberR(TestCase testCase)
         {
             try
             {
-                var actual = PrimeNumber(testCase.X);
+                ulong actual = FibRec(testCase.X);
 
-                if (string.Compare( actual, testCase.Expected) == 0)
+                if (actual == testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException != null)
+                {
+                    //TODO add type exception tests;
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+        }
+
+        static void TestNumberF(TestCase testCase)
+        {
+            try
+            {
+                ulong actual = FibFor(testCase.X);
+
+                if (actual == testCase.Expected)
                 {
                     Console.WriteLine("VALID TEST");
                 }
@@ -48,96 +76,149 @@ namespace Task1
         }
 
         /// <summary>
-        /// строковое представление результатов работы процедуры PrimeNumber
-        /// </summary>
-        private static string NotPrime = "НЕ ПРОСТОЕ";
-        private static string Prime = "ПРОСТОЕ";
-
-        /// <summary>
-        /// метод определения простое число введено пользователем или нет
+        /// метод рекурсивного вычисления числа Фибоначчи
         /// </summary>
         /// <param name="num"></param>
         /// <returns></returns>
-        static string PrimeNumber(int num)
+        static ulong calcRec(ulong p1, ulong p2, ulong n)
         {
-            string resume = NotPrime;
-            int d = 0;
-            int i = 2;
-            while (i < num)
-            {
-                if (num % i == 0)
-                    d++;
-                i++;
-            }
-
-            if (d == 0)
-                resume = Prime;
-
-            return resume;
+            return n == 0 ? p1 : calcRec(p2, p1 + p2, n - 1);
         }
+        static ulong FibRec(ulong n) { return calcRec(0, 1, n); }
+        static ulong calcFor(ulong p1, ulong p2, ulong n)
+        //int calcFor(int n, int p1 = 0, int p2 = 1)
+        {
+            if (n <= 1) return p1;
+            ulong p;
+            /*
+            for (ulong j = 2; j < n; j++)
+            {
+                p = p1;
+                p1 = p2;
+                p2 = p2 + p;
+            }/**/
 
-    static void Main(string[] args)
+            while (n > 0)
+            {
+                p = p1;
+                p1 = p2;
+                p2 = p2 + p;
+                n--;
+            }
+            return p1;
+        }
+        static ulong FibFor(ulong n) { return calcFor(0, 1, n); }
+
+        static void Main(string[] args)
         {
             Console.WriteLine("Программа проверки простое число или нет.");
             Console.WriteLine("Введите целое число: ");
-            int askNum = 0;
-            bool success = Int32.TryParse(Console.ReadLine(), out askNum);
-            // проверяемое число должно быть целым, иначе генерируем исключение
+            ulong askNum = 0;
+            bool success = ulong.TryParse(Console.ReadLine(), out askNum);
             if (!success)
             {
                 throw new ArgumentException("Не является целым");
             }
 
-            // проводим проверку
-            string checkNumber = PrimeNumber(askNum);
+            Console.WriteLine($"Введено число {askNum} ");
 
-            Console.WriteLine($"Введено {checkNumber} число {askNum} ");
+            Console.WriteLine($"Число Фибоначчи = {FibRec(askNum)} (вычислено рекурсивно)");
+            Console.WriteLine($"Число Фибоначчи = {FibFor(askNum)} (вычислено в цикле)");
 
-            // проводим тесты корректности работы процедуры PrimeNumber
+            Console.WriteLine("Проведение тестов вычисления числа Фибоначчи рекурсивным методом");
+
             var testCase = new TestCase()
             {
                 X = 4,
-                Expected = NotPrime,
+                Expected = 3,
                 ExpectedException = null
             };
 
-            TestPrimeNumber(testCase);
+            TestNumberR(testCase);
 
             testCase = new TestCase()
             {
-                X = 6,
-                Expected = Prime,
+                X = 7,
+                Expected = 13,
                 ExpectedException = null
             };
 
-            TestPrimeNumber(testCase);
+            TestNumberR(testCase);
 
             testCase = new TestCase()
             {
-                X = 3,
-                Expected = Prime,
+                X = 18,
+                Expected = 2584,
                 ExpectedException = null
             };
 
-            TestPrimeNumber(testCase);
+            TestNumberR(testCase);
 
             testCase = new TestCase()
             {
-                X = 823,
-                Expected = NotPrime,
+                X = 4,
+                Expected = 4,
                 ExpectedException = null
             };
 
-            TestPrimeNumber(testCase);
+            TestNumberR(testCase);
 
             testCase = new TestCase()
             {
-                X = 823,
-                Expected = Prime,
+                X = 18,
+                Expected = 2565,
                 ExpectedException = null
             };
 
-            TestPrimeNumber(testCase);
+            TestNumberR(testCase);
+
+            Console.WriteLine("Проведение тестов вычисления числа Фибоначчи с помощью цикла");
+
+            testCase = new TestCase()
+            {
+                X = 4,
+                Expected = 3,
+                ExpectedException = null
+            };
+
+            TestNumberF(testCase);
+
+            testCase = new TestCase()
+            {
+                X = 7,
+                Expected = 13,
+                ExpectedException = null
+            };
+
+            TestNumberF(testCase);
+
+            testCase = new TestCase()
+            {
+                X = 18,
+                Expected = 2584,
+                ExpectedException = null
+            };
+
+            TestNumberF(testCase);
+
+            testCase = new TestCase()
+            {
+                X = 4,
+                Expected = 4,
+                ExpectedException = null
+            };
+
+            TestNumberF(testCase);
+
+            testCase = new TestCase()
+            {
+                X = 18,
+                Expected = 2565,
+                ExpectedException = null
+            };
+
+            TestNumberF(testCase);
+            /**/
         }
     }
 }
