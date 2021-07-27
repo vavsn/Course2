cf9480ba0a9d64cbf444963eac8db3b0474bc421
vavsn <41681585+vavsn@users.noreply.github.com>
2021-07-27 20:12:40 +0700

    Lesson 8. Homework. Sorting an array using the metod Bucket Sort

--- Program.cs
+++ Program.cs
@@ -1,143 +1,89 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Task1
+namespace BucketSort
 {
+
     class Program
     {
         /// <summary>
-        /// класс для проведения тестирования
+        /// метод сортировки массива
         /// </summary>
-        public class TestCase
-        {
-            public int X { get; set; }
-            public string Expected { get; set; }
-            public Exception ExpectedException { get; set; }
-        }
-
-        /// <summary>
-        /// метод проведения теста корректности работы процедуры PrimeNumber
-        /// </summary>
-        /// <param name="testCase"></param>
-        static void TestPrimeNumber(TestCase testCase)
-        {
-            try
-            {
-                var actual = PrimeNumber(testCase.X);
-
-                if (string.Compare( actual, testCase.Expected) == 0)
-                {
-                    Console.WriteLine("VALID TEST");
-                }
-                else
-                {
-                    Console.WriteLine("INVALID TEST");
-                }
-            }
-            catch (Exception ex)
-            {
-                if (testCase.ExpectedException != null)
-                {
-                    //TODO add type exception tests;
-                    Console.WriteLine("VALID TEST");
-                }
-                else
-                {
-                    Console.WriteLine("INVALID TEST");
-                }
-            }
-        }
-
-        /// <summary>
-        /// строковое представление результатов работы процедуры PrimeNumber
-        /// </summary>
-        private static string NotPrime = "НЕ ПРОСТОЕ";
-        private static string Prime = "ПРОСТОЕ";
-
-        /// <summary>
-        /// метод определения простое число введено пользователем или нет
-        /// </summary>
-        /// <param name="num"></param>
+        /// <param name="x"></param>
         /// <returns></returns>
-        static string PrimeNumber(int num)
+        public static List<int> Sort(int[] x)
         {
-            string resume = NotPrime;
-            int d = 0;
-            int i = 2;
-            while (i < num)
+            List<int> sortedArray = new List<int>();
+
+            int numBuck = 10;
+
+            List<int>[] buckets = new List<int>[numBuck];
+            for (int i = 0; i < numBuck; i++)
             {
-                if (num % i == 0)
-                    d++;
-                i++;
+                buckets[i] = new List<int>();
             }
 
-            if (d == 0)
-                resume = Prime;
+            for (int i = 0; i < x.Length; i++)
+            {
+                int bucket = (x[i] / numBuck);
+                buckets[bucket].Add(x[i]);
+            }
 
-            return resume;
+            for (int i = 0; i < numBuck; i++)
+            {
+                List<int> temp = SortBucket(buckets[i]);
+                sortedArray.AddRange(temp);
+            }
+            return sortedArray;
         }
 
-    static void Main(string[] args)
+        /// <summary>
+        /// метод сортирует содержимое корзины
+        /// </summary>
+        /// <param name="inp"></param>
+        /// <returns></returns>
+        public static List<int> SortBucket(List<int> inp)
         {
-            Console.WriteLine("Программа проверки простое число или нет.");
-            Console.WriteLine("Введите целое число: ");
-            int askNum = 0;
-            bool success = Int32.TryParse(Console.ReadLine(), out askNum);
-            // проверяемое число должно быть целым, иначе генерируем исключение
-            if (!success)
+            for (int i = 1; i < inp.Count; i++)
             {
-                throw new ArgumentException("Не является целым");
+                int curValue = inp[i];
+                int pnt = i - 1;
+
+                while (pnt >= 0)
+                {
+                    if (curValue < inp[pnt])
+                    {
+                        inp[pnt + 1] = inp[pnt];
+                        inp[pnt] = curValue;
+                    }
+                    else break;
+                }
             }
 
-            // проводим проверку
-            string checkNumber = PrimeNumber(askNum);
+            return inp;
+        }
 
-            Console.WriteLine($"Введено {checkNumber} число {askNum} ");
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 6, 56, 84, 12, 3, 11, 29, 55, 49, 21, 77, 92, 81, 14, 66 };
+            
+            Console.WriteLine("Сорировка методом \"Bucket Sort\"");
+            Console.WriteLine("Массив чисел ДО сортировки");
 
-            // проводим тесты корректности работы процедуры PrimeNumber
-            var testCase = new TestCase()
+            foreach (var a in array)
             {
-                X = 4,
-                Expected = NotPrime,
-                ExpectedException = null
-            };
+                Console.Write(" " + a.ToString());
+            }
 
-            TestPrimeNumber(testCase);
+            List<int> sortedList = Sort(array);
 
-            testCase = new TestCase()
+            Console.WriteLine();
+            Console.WriteLine("Результаты сортировки");
+            foreach (var a in sortedList)
             {
-                X = 6,
-                Expected = Prime,
-                ExpectedException = null
-            };
+                Console.Write(" " + a.ToString());
+            }
 
-            TestPrimeNumber(testCase);
-
-            testCase = new TestCase()
-            {
-                X = 3,
-                Expected = Prime,
-                ExpectedException = null
-            };
-
-            TestPrimeNumber(testCase);
-
-            testCase = new TestCase()
-            {
-                X = 823,
-                Expected = NotPrime,
-                ExpectedException = null
-            };
-
-            TestPrimeNumber(testCase);
-
-            testCase = new TestCase()
-            {
-                X = 823,
-                Expected = Prime,
-                ExpectedException = null
-            };
-
-            TestPrimeNumber(testCase);
         }
     }
 }
